a0e1eb128f791f6b4d8abf77a49df440a5f108e0
SigureRuri <50079507+SigureRuri@users.noreply.github.com>
2021-08-31 18:46:54 +0900

    EnderCrystalにisShowingBottomを追加する

--- src/main/java/com/github/sigureruri/arcticicefloes/entity/AIFEnderCrystal.kt
+++ src/main/java/com/github/sigureruri/arcticicefloes/entity/AIFEnderCrystal.kt
@@ -1,12 +1,23 @@
 package com.github.sigureruri.arcticicefloes.entity
 
 import org.bukkit.Location
+import org.bukkit.entity.EnderCrystal
 import org.bukkit.entity.Entity
 import org.bukkit.entity.EntityType
 
 abstract class AIFEnderCrystal(id: ArcticIceFloesEntityId) : AIFEntity(id) {
 
+    open val isShowingBottom = true
+
     override fun spawnBaseEntity(location: Location): Entity? =
         location.world?.spawnEntity(location, EntityType.ENDER_CRYSTAL)
+
+    override fun applyEntity(entity: Entity) {
+        super.applyEntity(entity)
+
+        if (entity !is EnderCrystal) return
+
+        entity.isShowingBottom = isShowingBottom
+    }
 
 }
